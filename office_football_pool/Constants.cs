9480482f5a97158929126afe5dd120865aea7c96
UtterCaOS <uttercaos@interdentity.com>
2015-10-22 11:03:36 -0700

    Initial check-in of module office_football_pool

---
+++ office_football_pool/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace office_football_pool
+{
+	public static class Constants
+	{
+		// Replace strings with your mobile services and gateway URLs.
+		public static string ApplicationURL = @"https://office-football-pool.azurewebsites.net";
+        public static string GatewayURL = @"https://default-web-westus92e089a0c9724cc3bbaff2e0d87c7727.azurewebsites.net"; 
+	}
+}
+
---
+++ office_football_pool/TodoItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace office_football_pool
+{
+	public class TodoItem
+	{
+		string id;
+		string name;
+		string notes;
+		bool done;
+
+		[JsonProperty(PropertyName = "id")]
+		public string ID
+		{
+			get { return id; }
+			set { id = value;}
+		}
+
+		[JsonProperty(PropertyName = "text")]
+		public string Name
+		{
+			get { return name; }
+			set { name = value;}
+		}
+
+		[JsonProperty(PropertyName = "notes")]
+		public string Notes
+		{
+			get { return notes; }
+			set { notes = value;}
+		}
+
+		[JsonProperty(PropertyName = "complete")]
+		public bool Done
+		{
+			get { return done; }
+			set { done = value;}
+		}
+
+        [Version]
+        public string Version { get; set; }
+	}
+}
+
